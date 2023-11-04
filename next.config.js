5c7bf85989299e5fb4ed6e8a0313535c941d13e6
NoumaanAhamed <noumaanahamed@gmail.com>
2023-11-05 03:41:30 +0530

    first;

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains:["localhost","images.unsplash.com","lh3.googleusercontent.com","unsplash.com","th.bing.com"],
     remotePatterns: [
       { hostname: "images.unsplash.com" },
       { hostname: "lh3.googleusercontent.com" },
+      { hostname: "unsplash.com"},
+      { hostname: "imgs.search.brave.com"}
+      ,{ hostname: "th.bing.com"}
     ],
   },
   experimental: {
